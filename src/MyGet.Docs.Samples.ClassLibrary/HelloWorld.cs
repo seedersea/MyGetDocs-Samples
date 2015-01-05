02d88809040904d9fee042e4c6e92dfb3a715ac8
Xavier Decoster <xavier.decoster@outlook.com>
2015-01-05 14:31:49 +0100

    created simple class library with tokenized nuspec

---
+++ src/MyGet.Docs.Samples.ClassLibrary/HelloWorld.cs
@@ -0,0 +1,10 @@
+﻿namespace MyGet.Docs.Samples
+{
+    public static class NiceGuy
+    {
+        public static string SayHelloWorld()
+        {
+            return "Hello world";
+        }
+    }
+}
---
+++ src/MyGet.Docs.Samples.ClassLibrary/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyGet.Docs.Samples.ClassLibrary")]
+[assembly: AssemblyDescription("Sample class library project.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MyGet Ltd")]
+[assembly: AssemblyProduct("MyGet.Docs.Samples")]
+[assembly: AssemblyCopyright("Copyright © MyGet Ltd 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("51348b52-510e-4710-af28-4334bbd2d625")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.*")]
